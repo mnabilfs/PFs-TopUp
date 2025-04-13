849727b060db85a5a491527ce1714bbe3a8a2e80
mnabilfs <mnabilfs22@gmail.com>
2025-04-13 15:33:04 +0700

    install flowbite-react

--- src/pages/Topup_ml.jsx
+++ src/pages/Topup_ml.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Button } from "flowbite-react";
 
 const Topup_ml = () => {
   return (
-    <div className='bg-gray-900 h-screen w-full'>
-        <div className="flex justify-center items-center h-screen">
-        <div className='text-white font-bold text-4xl'>Hai ini topup_ml</div>
+    <div className="bg-gray-900 h-screen w-full">
+      <div className="flex flex-col justify-center items-center gap-5 h-screen ">
+        <div className="text-white font-bold text-4xl">
+          Hai ini topup_ml
         </div>
+          <Button className="cursor-pointer">Click me</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Topup_ml
+export default Topup_ml;
