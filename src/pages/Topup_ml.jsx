d76afb9d5f39a4f60238eb8e6fa40f5a8196242f
mnabilfs <mnabilfs22@gmail.com>
2025-04-13 12:34:39 +0700

    add style topup_ml

--- src/pages/Topup_ml.jsx
+++ src/pages/Topup_ml.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Topup_ml = () => {
   return (
-    <div>Hai ini topup_ml</div>
+    <div className='bg-gray-900 h-screen w-full'>
+        <div className="flex justify-center items-center h-screen">
+        <div className='text-white font-bold text-4xl'>Hai ini topup_ml</div>
+        </div>
+    </div>
   )
 }
 
