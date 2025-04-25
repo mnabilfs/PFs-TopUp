4b167ffa19cd5ad5ebb26f46c1874058f70092d9
SaKaSe2 <gmrikza05@gmail.com>
2025-04-26 00:45:50 +0700

    New

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,8 @@ const Home = () => {
     <div className="flex flex-col min-h-screen bg-gray-800">
       <Navbar />
 
-      <div className="px-4 md:px-30 mt-13">
+      <div className="px-4 md:px-26 mt-11">
+
         <div className="grid grid-cols-1 md:grid-cols-3 gap-11">
           
           <div className="md:col-span-2 relative">
@@ -78,6 +79,94 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <div className="px-4 md:px-30 mt-11">
+  <h2 className="flex items-center text-white text-xl font-semibold mb-4">
+    <img
+      src="https://www.transparentpng.com/thumb/diamond/O3UOts-diamond-best-png.png"
+      alt="Diamond"
+      className="h-6 w-6 md:h-8 md:w-8 mr-2"
+    />
+    Top Up Game
+  </h2>
+  <div className="flex gap-19 overflow-x-auto pb-4">
+
+    <div className="flex-shrink-0 cursor-pointer">
+      <div className="bg-purple-900 rounded-lg overflow-hidden">
+        <img
+          src="https://play-lh.googleusercontent.com/ovqnOQDL8SPTocGIfDpZCJrsRfr4063AXkoG4xOTf7SPj6FPxmPEA5ChsFf9bOCB4bI"
+          alt="Mobile Legends"
+          className="w-16 h-16 md:w-40 md:h-40 object-cover"
+        />
+        <div className="px-2 py-1 text-center">
+          <span className="text-white text-xs font-semibold">
+            Mobile Legends
+          </span>
+        </div>
+      </div>
+    </div>
+    
+    <div className="flex-shrink-0 cursor-pointer">
+      <div className="bg-purple-900 rounded-lg overflow-hidden">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8EAG9DqC3a_aGUkcMYOtMC1IwnMWxOUpQvQ&s"
+          alt="PUBG Mobile"
+          className="w-16 h-16 md:w-40 md:h-40 object-cover"
+        />
+        <div className="px-2 py-1 text-center">
+          <span className="text-white text-xs font-semibold">
+            PUBG Mobile
+          </span>
+        </div>
+      </div>
+    </div>
+    
+    <div className="flex-shrink-0 cursor-pointer">
+      <div className="bg-purple-900 rounded-lg overflow-hidden">
+        <img
+          src="https://play-lh.googleusercontent.com/40e-a0ZjIvFUrBPCuw2dpfJY--8jCPJxVW6dMrQ1EBl8LYhX7ZdC9DpUtg69Joq8x6U"
+          alt="Honkai Impact 3rd"
+          className="w-16 h-16 md:w-40 md:h-40 object-cover"
+        />
+        <div className="px-2 py-1 text-center">
+          <span className="text-white text-xs font-semibold">
+            Honkai Impact 3rd
+          </span>
+        </div>
+      </div>
+    </div>
+    
+    <div className="flex-shrink-0 cursor-pointer">
+      <div className="bg-purple-900 rounded-lg overflow-hidden">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrIOTjQn295QJG51JoDtCe-WZisHNK2j2d8g&s"
+          alt="Genshin Impact"
+          className="w-16 h-16 md:w-40 md:h-40 object-cover"
+        />
+        <div className="px-2 py-1 text-center">
+          <span className="text-white text-xs font-semibold">
+            Genshin Impact
+          </span>
+        </div>
+      </div>
+    </div>
+    
+    <div className="flex-shrink-0 cursor-pointer">
+      <div className="bg-purple-900 rounded-lg overflow-hidden">
+        <img
+          src="https://yt3.googleusercontent.com/ekLwLEDxzfU9NRYD0dO30dOiet0gulvm4NX9evsyvmjyzo5vDnhokKTYXP03GSxp8SIviJyxTIQ=s900-c-k-c0x00ffffff-no-rj"
+          alt="Free Fire"
+          className="w-16 h-16 md:w-40 md:h-40 object-cover"
+        />
+        <div className="px-2 py-1 text-center">
+          <span className="text-white text-xs font-semibold">
+            Free Fire
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
       <FooterUniversal />
     </div>
