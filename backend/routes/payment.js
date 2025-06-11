65b00bbd512e9c77ef48a9bd3494aa2bfec8d571
mnabilfs <mnabilfs22@gmail.com>
2025-06-11 14:17:20 +0700

    fix backend for deployment

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createPayment, checkPaymentStatus, updateTransactionStatus } = require("../controllers/paymentController");
 
 router.post("/create", createPayment);
-router.get("/payment/status/:orderId", checkPaymentStatus);  // Untuk cek status transaksi
-router.put("/payment/status/:orderId", updateTransactionStatus);  // Untuk update status transaksi
+router.get("/status/:orderId", checkPaymentStatus);
+router.put("/status/:orderId", updateTransactionStatus);
 
 module.exports = router;
